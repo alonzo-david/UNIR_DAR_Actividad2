8a43d9bf1a9fe84bb83069a656c5b64def241749
david-alonzo <78769397+alonzo-david@users.noreply.github.com>
2024-11-04 15:48:15 -0600

    First Commit
    Activity 2 about a create a page with a framework

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1>Hello Word!!</h1>
+      <div className="card">
+        <p>Universidad Internacional de la Rioja en México</p>
+        <p>Desarrollo de Aplicaciones en Red</p>
+        <p>Actividad 2. Tutorial de una tecnología/framework</p>
+      </div>
+      <div>
+        <p className="read-the-docs">Grupo B247</p>
+        <p className="read-the-docs">David Judá Alonzo Monzón</p>
+        <p className="read-the-docs">Rodrigo Ramos Xochiteotzin</p>
+        <p className="read-the-docs">Alberto Serrato Cortez</p>
+        <p className="read-the-docs"></p>
+      </div>
+    </>
+  );
+}
+
+export default App;
